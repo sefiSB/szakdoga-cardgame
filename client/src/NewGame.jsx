83d7cd6c423248f0f8fa06ec754c1318b10aa350
Bálint Schäffer <balintsch7@gmail.com>
2025-02-10 13:45:25 +0100

    added server

---
+++ client/src/NewGame.jsx
@@ -0,0 +1,115 @@
+import { useState } from "react";
+import { initialState } from "./Store/store";
+
+function NewGame() {
+  const [isCardsOnDesk, setIsCardsOnDesk] = useState(false);
+
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center h-screen gap-4">
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">What's the game called?</span>
+            {/* <span className="label-text-alt">Top Right label</span> */}
+          </div>
+          <input
+            type="text"
+            placeholder="Name"
+            className="input input-bordered w-full max-w-xs"
+          />
+          {/* <div className="label">
+            <span className="label-text-alt">Bottom Left label</span>
+            <span className="label-text-alt">Bottom Right label</span>
+          </div> */}
+        </label>
+
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Number of starting cards in hand</span>
+            {/* <span className="label-text-alt">Top Right label</span> */}
+          </div>
+          <input
+            type="number"
+            placeholder="Place a number here"
+            className="input input-bordered w-full max-w-xs"
+          />
+          {/* <div className="label">
+            <span className="label-text-alt">Bottom Left label</span>
+            <span className="label-text-alt">Bottom Right label</span>
+          </div> */}
+        </label>
+
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Cards on desk/player</span>
+            {/* <span className="label-text-alt">Top Right label</span> */}
+          </div>
+          <input
+            type="checkbox"
+            className="toggle border-indigo-600 bg-indigo-500 checked:bg-orange-400 checked:text-orange-800 checked:border-orange-500 "
+            onClick={(e) => {
+              console.log(e.target.checked);
+              setIsCardsOnDesk(e.target.checked);
+            }}
+          />
+        </label>
+
+        {isCardsOnDesk ? (
+          <>
+            <div className="border p-5 rounded-3xl border-dashed border-orange-600">
+              
+              <label className="form-control w-full max-w-xs ">
+                <div className="label">
+                  <span className="label-text">
+                    Number of revealed cards on desk/player
+                  </span>
+                  {/* <span className="label-text-alt">Top Right label</span> */}
+                </div>
+                <input
+                  type="number"
+                  placeholder="Place a number here"
+                  className="input input-bordered w-full max-w-xs"
+                />
+                {/* <div className="label">
+            <span className="label-text-alt">Bottom Left label</span>
+            <span className="label-text-alt">Bottom Right label</span>
+          </div> */}
+              </label>
+
+              <label className="form-control w-full max-w-xs">
+                <div className="label">
+                  <span className="label-text">
+                    Number of hideen cards on desk/player
+                  </span>
+                  {/* <span className="label-text-alt">Top Right label</span> */}
+                </div>
+                <input
+                  type="number"
+                  placeholder="Place a number here"
+                  className="input input-bordered w-full max-w-xs"
+                />
+                {/* <div className="label">
+            <span className="label-text-alt">Bottom Left label</span>
+            <span className="label-text-alt">Bottom Right label</span>
+          </div> */}
+              </label>
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
+
+        <button
+          className="btn btn-outline btn-primary"
+          onClick={() => {
+            //itt kell majd egy check, majd az asztalhoz ültetés
+          }}
+        >
+          Submit
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default NewGame;
